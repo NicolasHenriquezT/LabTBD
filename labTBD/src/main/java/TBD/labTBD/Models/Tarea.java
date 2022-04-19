5b811e6c84a30d73c3a2be1b220076e4bebccc53
Marcelo <marcelo.guzman.g@usach.cl>
2022-04-19 16:49:49 -0400

    Agregar atributo faltante

--- labTBD/src/main/java/TBD/labTBD/Models/Tarea.java
+++ labTBD/src/main/java/TBD/labTBD/Models/Tarea.java
@@ -1,8 +1,11 @@
 package TBD.labTBD.Models;
+
 import java.sql.Timestamp;
+import java.util.List;
 
 public class Tarea {
     Timestamp fechaInicio;
     String estado, nombre, descripcion;
     String ubicacion;
+    List<String> listaHabilidades;
 }
