5d86ad1582f73d4cb872f0edd347a6d68a2a4261
Israel Guzman <israel.guzman@usach.cl>
2022-04-24 00:50:59 -0400

    Creación CRUD para la clase EMERGENCIA

---
+++ labTBD/src/main/java/TBD/labTBD/Models/Emergencia.java
@@ -0,0 +1,71 @@
+package TBD.labTBD.Models;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Emergencia {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int id_coordinador;
+    private String ubicacion;
+    private Timestamp fecha;
+    private List<Tarea> listaTareas;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_coordinador() {
+        return id_coordinador;
+    }
+
+    public void setId_coordinador(int id_coordinador) {
+        this.id_coordinador = id_coordinador;
+    }
+
+    public List<Tarea> getListaTareas() {
+        return listaTareas;
+    }
+
+    public void setListaTareas(List<Tarea> listaTareas) {
+        this.listaTareas = listaTareas;
+    }
+
+    public Timestamp getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Timestamp fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
